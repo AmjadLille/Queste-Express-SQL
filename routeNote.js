e862fcc949fb2a60087d0c3ab0be73bdc22532be
amjad <sammanamjad86@gmail.com>
2021-01-05 22:04:54 +0100

    fin

--- routeNote.js
+++ routeNote.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const connection = require("./connection");
 
 
-// GET all data
+// 1-- GET all data
 
-router.get("/", (req, res) => {
+router.get("/all", (req, res) => {
 
     connection.query("SELECT * FROM notes", (error, rows) => {
         if (rows.length === 0) {
@@ -16,11 +16,11 @@ router.get("/", (req, res) => {
     })
 });
 
-// GET - Retrieve specific fields 
+// 2-- GET - Retrieve specific fields 
 
-router.get("/:id", (req, res, next) => {
+router.get("/all/:id", (req, res, next) => {
 
-    connection.query('SELECT * FROM notes where id=?', req.params.id, (error, rows, fields) => {
+    connection.query('SELECT * FROM notes where id=?', req.params.id, (error, rows) => {
         if (error) {
             return next(error);
         }
@@ -32,34 +32,80 @@ router.get("/:id", (req, res, next) => {
     })
 })
 
-//GET - Retrieve a data set with the following :
-// filter for data that contains... 
-
-// filter for data that starts with...
-
-// filter for data that is greater than... 
-
-// GET - Ordered data recovery (i.e. ascending, descending) - The order should be passed as a route parameter
-
-router.get("/way=", (req, res, next) => {
-
-    connection.query('SELECT * FROM notes ORDER BY note ? ', [req.query.way], (error, rows, fields) => {
+//GET  - Retrieve a data set with the following :
+// 3-- filter for data that contains... 
+router.get("/filter1", (req, res) => {
+    const { contain } = req.query
+    connection.query(`SELECT * FROM notes where note like '%${contain}%'`, (error, rows) => {
         if (error) {
-            return next(error);
-        }
-        if (rows.length === 0) {
-            res.status(404).send({ error: "Note not found" });
+            console.log(error);
+            res.status(500).send("Error retrieving data");
+
+        } else if (rows.length === 0) {
+            res.status(404).send("no text found for this filter");
         } else {
             res.status(200).json(rows);
         }
     })
 })
 
-// POST - Insertion of a new entity
+
+
+// 4-- filter for data that starts with...
+
+router.get("/filter2", (req, res) => {
+    const { start } = req.query
+    connection.query(`SELECT * FROM notes where note like '${start}%'`, (error, rows) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send("Error retrieving data");
+
+        } else if (rows.length === 0) {
+            res.status(404).send("no text found for this filter");
+        } else {
+            res.status(200).json(rows);
+        }
+    })
+})
+
+//5-- filter for data that is greater than... 
+
+router.get("/date", (req, res) => {
+
+    connection.query(`SELECT * FROM notes where added_at >= ?`, [req.query.date], (error, rows) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send("Error retrieving data");
+
+        } else if (rows.length === 0) {
+            res.status(404).send("no text found for this filter");
+        } else {
+            res.status(200).json(rows);
+        }
+    })
+})
+
+//6-- GET - Ordered data recovery (i.e. ascending, descending) - The order should be passed as a route parameter
+
+router.get("/order", (req, res, next) => {
+    const { way } = req.query
+
+    connection.query(`SELECT * FROM notes ORDER BY note ${way}`, (error, rows) => {
+        if (error) {
+            return next(error);
+
+        } else {
+            res.status(200).json(rows);
+        }
+    })
+})
+
+
+//7-- POST - Insertion of a new entity
 
 router.post("/", (req, res) => {
     const { note, date, done } = req.body;
-    connection.query("INSERT INTO notes(note, date, done) VALUES(?, ?, ?)", [note, date, done], (err, results) => {
+    connection.query("INSERT INTO notes(note, date, done) VALUES(?, ?, ?)", [note, date, done], (err, rows) => {
         if (err) {
             console.log(err);
             res.status(500).send("Error saving a note");
@@ -70,14 +116,14 @@ router.post("/", (req, res) => {
 });
 
 
-// PUT - Modification of an entity
+//8-- PUT - Modification of an entity
 
 router.put("/:id", (req, res) => {
     const idNote = req.params.id;
 
     const newNote = req.body;
 
-    connection.query("UPDATE notes SET ? WHERE id = ?", [newNote, idNote], (err, results) => {
+    connection.query("UPDATE notes SET ? WHERE id = ?", [newNote, idNote], (err, rows) => {
         if (err) {
             console.log(err);
             res.status(500).send("Error updating a note");
@@ -87,12 +133,12 @@ router.put("/:id", (req, res) => {
     });
 });
 
-// PUT - Toggle a Boolean value
+//9-- PUT - Toggle a Boolean value
 
 router.put("/toggle/:id", (req, res) => {
     const idNote = req.params.id;
 
-    connection.query("UPDATE notes SET done = NOT done WHERE id = ?", [idNote], (err, results) => {
+    connection.query("UPDATE notes SET done = NOT done WHERE id = ?", [idNote], (err, rows) => {
         if (err) {
             console.log(err);
             res.status(500).send("Error updating a note");
@@ -102,11 +148,11 @@ router.put("/toggle/:id", (req, res) => {
     });
 });
 
-// DELETE - Delete an entity
+//10-- DELETE - Delete an entity
 
 router.delete("/:id", (req, res) => {
 
-    connection.query('DELETE FROM notes WHERE id = ?', [req.params.id], (error, rows, fields) => {
+    connection.query("DELETE FROM notes WHERE id = ?", [req.params.id], (error, rows) => {
         if (error) {
             console.log(error);
             res.status(500).send("Error deleting a Note");
@@ -117,6 +163,17 @@ router.delete("/:id", (req, res) => {
     })
 })
 
-// DELETE - Delete all entities where boolean value is false
+// 11-- DELETE - Delete all entities where boolean value is false
+router.delete("/done", (req, res) => {
 
+    connection.query("DELETE FROM notes WHERE done = 0", (error, rows) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send("Error deleting a Note");
+        }
+        else {
+            res.status(200).send(" Note deleted!");
+        }
+    })
+})
 module.exports = router
